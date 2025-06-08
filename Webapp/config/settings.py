4c38cd991c0b684e1120e83092fbd68bd1681d7a
Biswajit Bhoi <biswajit.bhoi@gmail.com>
2025-06-08 21:24:57 +0530

    testing the changes

--- Webapp/config/settings.py
+++ Webapp/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+orpt#u2^ji-_o%5^qv-r^n*epm4-uu+@g-xmtxk_j^q)7iv8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
